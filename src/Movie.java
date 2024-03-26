62ee43c13292d7b8599453005bc483acd51af2e4
Saenro Gonsalves <aenros09602@gmail.com>
2024-03-26 15:24:45 +0530

    file createed

---
+++ src/Movie.java
@@ -0,0 +1,12 @@
+public class Movie {
+    private string title;
+    private String director;
+    public Movie (String title,String director) {
+        this.title = title;
+        this.director=director;
+    }
+    public String getTitle(){
+        return this.Title;
+
+    }
+}
